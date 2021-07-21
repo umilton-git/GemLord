98b7c6c8247decaa456554b993ae1dc6d0ac38a8
Ulysses Milton <ulysses.github@protonmail.com>
2021-07-21 16:39:12 -0500

    Started simple movement system

---
+++ Assets/Components/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 30f;
+
+    void Update()
+    {
+        if (this.enabled == true)
+        {
+                    if (Input.GetKey("left shift"))
+        {
+            speed = 60f;
+        }
+        else
+        {
+            speed = 30f;
+        }
+        
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+        }
+    }
+}
