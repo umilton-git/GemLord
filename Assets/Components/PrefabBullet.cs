efa7786451e94264edd6e97f39493e74685fbb93
Ulysses Milton <ulysses.github@protonmail.com>
2021-07-22 01:09:42 -0500

    Added shooting system

---
+++ Assets/Components/PrefabBullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PrefabBullet : MonoBehaviour
+{
+    public Rigidbody theBullet;
+    public float Speed = 20f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var clone = Instantiate(theBullet, transform.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(0, 0, Speed);
+
+            Destroy(clone.gameObject, 3);
+        }
+        
+    }
+}
