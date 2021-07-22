5e2b0c8f9a92292505e20361351dd81d63573d03
Ulysses Milton <ulysses.github@protonmail.com>
2021-07-22 10:47:04 -0500

    Added first enemy and basic AI

---
+++ Assets/CrabHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabHealth : MonoBehaviour
+{
+    public float Health = 3;
+    // Start is called before the first frame update   
+    void OnCollisionEnter(Collision col)
+   {
+       if(col.gameObject.layer == 9)
+       {
+        Health -= 1;
+       }
+   }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Health == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/CrabMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class CrabMovement : MonoBehaviour
+{
+    public Transform Player;
+    public NavMeshAgent Enemy;
+    void Update()
+    {
+        Enemy.SetDestination(Player.position);
+        Debug.Log(Player.position);
+    }
+}
