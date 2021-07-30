b5b5593ac2505922ddf0be91d577cfcd71233bb0
Ulysses Milton <ulysses.github@protonmail.com>
2021-07-29 22:01:51 -0500

    Added damage to crab enemy contact

---
+++ Assets/DealDamageCrab.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamageCrab : MonoBehaviour
+{
+    public float damageCooldown = 3;
+    private float damageCooldownCount = 0;
+   void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("Crab Bite!");
+            var health = collision.GetComponent<Health>();
+            if(health != null && damageCooldownCount <= 0)
+            {
+                health.TakeDamage();
+                Debug.Log(health.currentHealth);
+                damageCooldownCount = damageCooldown;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(damageCooldownCount > 0)
+        {
+            damageCooldownCount = damageCooldownCount - Time.deltaTime;
+        }
+
+    }
+}
