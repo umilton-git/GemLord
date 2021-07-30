f2ef5c523de34500f1f8d2f6f94ec2eb0c11e2b2
umilton-git <ulysses.github@protonmail.com>
2021-07-30 10:34:26 -0500

    Fixed some errors in last update

---
+++ Assets/DestroyEnemyBullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemyBullet : MonoBehaviour
+{
+   void OnCollisionEnter(Collision col)
+   {
+      if(col.gameObject.layer != 11  && col.gameObject.layer != 10)
+      {
+        Destroy(this.gameObject);
+      }
+   }
+}
+
