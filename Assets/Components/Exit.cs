fa1fe1d872f29f4e6108367e2613d5d5f070771e
umilton-git <ulysses.github@protonmail.com>
2021-07-31 20:57:54 -0500

    Final touches, don't think I'll finish on time for this one

---
+++ Assets/Components/Exit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    void Start(){
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void exitgame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Components/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void Begin()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+}
+
